e6f0142b6965f86a84b68288c93d15ae91e44dad
Ivan Shaporov <ivshapo@microsoft.com>
2018-04-03 17:53:46 -0700

    Create scripts for creating MT app with the AAD password stored in KV and adding permissions for acs principal to access the password.

--- deployment/deployCluster.sh
+++ deployment/deployCluster.sh
@@ -4,7 +4,9 @@ set -x
 
 CLUSTER_NAME="" # Add Desired Cluster Name
 LOCATION="" # Specify Location of Cluster
+SUBSCRIPTION_ID="" # Add Azure Subscription ID
 
+TENANT_ID=$(az account show --query tenantId -o tsv)
 
 ## -------
 ## create resource group
@@ -14,13 +16,21 @@ az group create --name=$RESOURCE_GROUP --location=$LOCATION
 
 ## -------
 ## create service principal
-SUBSCRIPTION_ID="" # Add Azure Subscription ID
 SERVICE_PRINCIPAL_NAME=$CLUSTER_NAME
-SERVICE_PRINCIPAL_PASSWORD=`uuidgen``date | md5 | head -c10; echo`
 az ad sp delete --id http://$SERVICE_PRINCIPAL_NAME
-az ad sp create-for-rbac --name $SERVICE_PRINCIPAL_NAME --role="Contributor" --scopes="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$CLUSTER_NAME" --password $SERVICE_PRINCIPAL_PASSWORD -o json
+SERVICE_PRINCIPAL_PASSWORD=`az ad sp create-for-rbac --name $SERVICE_PRINCIPAL_NAME --role="Contributor" --scopes="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$CLUSTER_NAME" --query password -o tsv`
 
 sleep 5 # Azure CLI bug needs delay so SP can propegate
+
+## -------
+## create key vault for passing secrets down to the services
+KEYVAULT_NAME="$CLUSTER_NAME"-mt-deployment
+az keyvault create --name $KEYVAULT_NAME --resource-group $RESOURCE_GROUP --location $LOCATION
+
+SERVICE_PRINCIPAL_APP_ID=`az ad sp list --display-name ${SERVICE_PRINCIPAL_NAME} --query [].appId -o tsv`
+
+# SP running in K8s can only read the secret
+az keyvault set-policy --secret-permissions get --resource-group $RESOURCE_GROUP --name $KEYVAULT_NAME --spn http://$SERVICE_PRINCIPAL_NAME
 
 ## -------
 ## create kubernetes cluster
