d395032ae26c6b74964b246239b73f910c093b93
Elizabeth Halper <elhalper@microsoft.com>
2018-04-10 15:47:48 -0700

    OMS creating properly

--- deployment/globalVariables.prod.sh
+++ deployment/globalVariables.prod.sh
@@ -6,7 +6,7 @@ set -x
 # Global variables for the environment
 AZURE_SUBSCRIPTION_ID=4bfa739e-0d8c-437e-9302-e3da21c66828
 export AZURE_SUBSCRIPTION_ID
-AZURE_LOCATION=westus
+AZURE_LOCATION=eastus
 export AZURE_LOCATION
 PROJECT_NAME=microservices
 export PROJECT_NAME 
