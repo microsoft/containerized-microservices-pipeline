3ada755ec86c106019670c95ef3fa2417e3ab539
Aaron Schnieder <aaros@microsoft.com>
2018-04-12 16:18:56 -0700

    Improved inception and deployCluster scripts (#26)
    * Improved inception and deployCluster scripts
    Removed default variable values
    Added validation that required variables have values
    Writes output of inception and deployCluster to files
    
    * Changed ACR ID to ACR NAME
    
    * Removed unneccessary quotes

--- deployment/globalVariables.prod.sh
+++ deployment/globalVariables.prod.sh
@@ -4,14 +4,22 @@ set -x
 
 ## -------
 # Global variables for the environment
-AZURE_SUBSCRIPTION_ID=AzureSubscriptionIdGoesHere # Replace value with Azure Subscription ID to use
+AZURE_SUBSCRIPTION_ID= # Insert Azure Subscription ID value here
 export AZURE_SUBSCRIPTION_ID
-AZURE_LOCATION=eastus
+AZURE_LOCATION=eastus # Insert Azure Location here (westus, eastus, etc.)
 export AZURE_LOCATION
-PROJECT_NAME=microservices
+PROJECT_NAME= # Insert project name here
 export PROJECT_NAME 
 
 ## -------
 # Common Azure resources resource group
 COMMON_RESOURCE_GROUP=$PROJECT_NAME-$AZURE_LOCATION-resources
 export COMMON_RESOURCE_GROUP
+
+## -------
+# Validate that values have been set for required variables
+if [ -z "$AZURE_SUBSCRIPTION_ID" ] || [ -z "$AZURE_LOCATION" ] || [ -z "$PROJECT_NAME" ]
+then
+      echo "\A required value in globalVariables.prod.sh is empty!!!!!!!!!!!!!"
+      exit 1
+fi
