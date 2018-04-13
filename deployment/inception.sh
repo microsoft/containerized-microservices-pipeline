928c97165c550fba5fee8a5a865067e04b46a2b5
Aaron Schnieder <aaros@microsoft.com>
2018-04-13 15:25:13 -0700

    Fixed bugs in creation scripts

--- deployment/inception.sh
+++ deployment/inception.sh
@@ -59,7 +59,7 @@ echo ........ Creating App Insights
 ## -------
 # Create OMS Workspace
 echo ........ Creating OMS Workspace
-K8_DEPLOYMENT_DIAGSA_NAME=$PROJECT_NAMEdiagsa
+K8_DEPLOYMENT_DIAGSA_NAME="${PROJECT_NAME}diagsa"
 az storage account delete --name $K8_DEPLOYMENT_DIAGSA_NAME --resource-group $COMMON_RESOURCE_GROUP --yes
 az storage account create --name $K8_DEPLOYMENT_DIAGSA_NAME --resource-group $COMMON_RESOURCE_GROUP --location eastus --sku Standard_LRS
 az group deployment delete --resource-group $COMMON_RESOURCE_GROUP --name "Microsoft.LogAnalyticsOMS"
