88e7a5bba94ac8294324e81651801afa412685ed
Yutang Lin <yutanglin16@gmail.com>
2018-04-17 08:50:45 -0700

    Refactored code

---
+++ WebAndLoadTests/MTApi/MTApi.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace MTApi
+{
+    public class MTApiFunctionalities
+    {
+        public string loginApiRoute = "/api/login";
+        public string accountApiRoute = "/api/account";
+
+        public void deleteUser(string mtUrl, string userId)
+        {
+            // Delete user - uses log in token for user0 and the user id of the user to be deleted.
+            HttpWebRequest requestDeleteUser = (HttpWebRequest)WebRequest.Create(mtUrl + accountApiRoute + "/" + userId);
+            requestDeleteUser.Method = "DELETE";
+            JObject jsonResponse = loginUser(mtUrl, "user0", "Password0");
+            requestDeleteUser.Headers[HttpRequestHeader.Authorization] = "Bearer " + jsonResponse["token"];
+            var httpResponseDeleteUser = (HttpWebResponse)requestDeleteUser.GetResponse();
+        }
+
+        public JObject loginUser(string mtUrl, string userName, string password)
+        {
+            // Login as user0 (needs the log in token to delete other users)
+            HttpWebRequest requestLogin = (HttpWebRequest)WebRequest.Create(mtUrl + loginApiRoute);
+            requestLogin.Method = "POST";
+            requestLogin.ContentType = "application/json";
+            using (var streamWriter1 = new StreamWriter(requestLogin.GetRequestStream()))
+            {
+                string json = "{\"userName\":\"" + userName + "\", \"password\":\"" + password + "\"}";
+
+                streamWriter1.Write(json);
+                streamWriter1.Flush();
+                streamWriter1.Close();
+            }
+
+            var httpResponseLogin = (HttpWebResponse)requestLogin.GetResponse();
+
+            using (var streamReader = new StreamReader(httpResponseLogin.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                Console.WriteLine(result);
+                JObject json = JObject.Parse(result);
+                return json;
+            }
+        }
+    }
+}
--- WebAndLoadTests/WebAndLoadTests/CreateUserWebTest.cs
+++ WebAndLoadTests/WebAndLoadTests/CreateUserWebTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using Microsoft.VisualStudio.TestTools.WebTesting;
+using MTApi;
 using Newtonsoft.Json.Linq;
 using WebAndLoadTests.Properties;
 
@@ -10,7 +11,7 @@ namespace WebAndLoadTests
 {
     public class CreateUserWebTest : WebTest
     {
-        public string MTUrl = Settings.Default.MTUrl;
+        public string mtUrl = Settings.Default.MTUrl;
         public string loginApiRoute = "/api/login";
         public string accountApiRoute = "/api/account";
 
@@ -25,7 +26,7 @@ namespace WebAndLoadTests
         public override IEnumerator<WebTestRequest> GetRequestEnumerator()
         {
             // Create user test
-            WebTestRequest request1 = new WebTestRequest(MTUrl + accountApiRoute);
+            WebTestRequest request1 = new WebTestRequest(mtUrl + accountApiRoute);
             request1.Method = "POST";
             StringHttpBody request1Body = new StringHttpBody();
             request1Body.ContentType = "application/json";
@@ -39,60 +40,9 @@ namespace WebAndLoadTests
 
         private void TearDown(object sender, PostWebTestEventArgs e)
         {
-            HttpWebRequest requestLogin = (HttpWebRequest)WebRequest.Create(MTUrl + loginApiRoute);
-            requestLogin.Method = "POST";
-            requestLogin.ContentType = "application/json";
-            using (var streamWriter = new StreamWriter(requestLogin.GetRequestStream()))
-            {
-                string json = ""; // TODO: Json body - example : "{\"userName\":\"USERNAME\", \"password\":\"PASSWORD\"}";
-
-                streamWriter.Write(json);
-                streamWriter.Flush();
-                streamWriter.Close();
-            }
-
-            var httpResponseLogin = (HttpWebResponse)requestLogin.GetResponse();
-            JToken token;
-            JToken userId;
-
-            using (var streamReader = new StreamReader(httpResponseLogin.GetResponseStream()))
-            {
-                var result = streamReader.ReadToEnd();
-                Console.WriteLine(result);
-                JObject json = JObject.Parse(result);
-                token = json["token"];
-                userId = json["id"];
-            }
-
-            // Login as user0 (needs the log in token to delete other users)
-            HttpWebRequest requestLoginUser0 = (HttpWebRequest)WebRequest.Create(MTUrl + loginApiRoute);
-            requestLoginUser0.Method = "POST";
-            requestLoginUser0.ContentType = "application/json";
-            using (var streamWriter1 = new StreamWriter(requestLoginUser0.GetRequestStream()))
-            {
-                string json = "{\"userName\":\"user0\", \"password\":\"Password0\"}";
-
-                streamWriter1.Write(json);
-                streamWriter1.Flush();
-                streamWriter1.Close();
-            }
-
-            var httpResponseLoginUser0 = (HttpWebResponse)requestLoginUser0.GetResponse();
-            JToken user0Token;
-            
-            using (var streamReader = new StreamReader(httpResponseLoginUser0.GetResponseStream()))
-            {
-                var result = streamReader.ReadToEnd();
-                Console.WriteLine(result);
-                JObject json = JObject.Parse(result);
-                user0Token = json["token"];
-            }
-
-            // Delete user - uses log in token for user0 and the user id of the user to be deleted.
-            HttpWebRequest requestDeleteUser = (HttpWebRequest)WebRequest.Create(MTUrl + accountApiRoute + "/" + userId);
-            requestDeleteUser.Method = "DELETE";
-            requestDeleteUser.Headers[HttpRequestHeader.Authorization] = "Bearer " + user0Token;
-            var httpResponseDeleteUser = (HttpWebResponse)requestDeleteUser.GetResponse();
+            MTApiFunctionalities mtApi = new MTApiFunctionalities();
+            JObject jsonResponse = mtApi.loginUser(mtUrl, "user1x", "Password1x");
+            mtApi.deleteUser(mtUrl, jsonResponse["id"].ToString());
         }
 
 
