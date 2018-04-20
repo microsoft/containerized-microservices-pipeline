934bc52cb9aff006324088c9428929fce7844b6c
Arjun Jobanputra <ajobanpu@microsoft.com>
2018-04-20 13:36:56 -0700

    Added node pool variable.

--- deployment/scaleCluster.sh
+++ deployment/scaleCluster.sh
@@ -14,12 +14,12 @@ exec 2>&1
 RESOURCE_GROUP=$CLUSTER_NAME
 DNS_PREFIX=$CLUSTER_NAME
 NODE_COUNT= # Add desired node count
+NODE_POOL=agentpool1 #value should match the value in clusterDefinition.json
 
-## node-pool value should match the value in clusterDefinition.json
 acs-engine scale --subscription-id $AZURE_SUBSCRIPTION_ID \
     --resource-group $RESOURCE_GROUP  \
     --location $AZURE_LOCATION     \
     --deployment-dir _output/$DNS_PREFIX \
     --new-node-count $NODE_COUNT     \
-    --node-pool agentpool1 \
+    --node-pool $NODE_POOL \
     --master-FQDN $DNS_PREFIX.$AZURE_LOCATION.cloudapp.azure.com
