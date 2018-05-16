aff474ab919224c8a60b1ddef0d19d57162b4db9
Arjun Jobanputra <@>
2018-05-16 18:14:18 -0400

    Checking for long project names

--- deployment/globalVariables.prod.sh
+++ deployment/globalVariables.prod.sh
@@ -9,7 +9,7 @@ export AZURE_SUBSCRIPTION_ID
 AZURE_LOCATION=eastus # Insert Azure Location here (westus, eastus, etc.)
 export AZURE_LOCATION
 PROJECT_NAME= # Insert project name here
-export PROJECT_NAME 
+export PROJECT_NAME
 PUBLIC_DOMAIN_NAME_SUFFIX= # Application and middle tier will be accessible at $PROJECT_NAME.$PUBLIC_DOMAIN_NAME_SUFFIX.trafficmanager.net
 export PUBLIC_DOMAIN_NAME_SUFFIX
 
@@ -24,4 +24,12 @@ if [ -z "$AZURE_SUBSCRIPTION_ID" ] || [ -z "$AZURE_LOCATION" ] || [ -z "$PROJECT
 then
       echo "\A required value in globalVariables.prod.sh is empty!!!!!!!!!!!!!"
       exit 1
+fi
+
+# Storage account names are based on project name
+MAX_LENGTH=18
+if [ ${#PROJECT_NAME} -ge $MAX_LENGTH ]
+then
+      echo "$PROJECT_NAME cannot be greater than $MAX_LENGTH characters."
+      #exit 1
 fi
